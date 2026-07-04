d6ed64e9d9f9d1b5a42944c9873d4c3b1e1aaab8
Joshua Gato <joshuagato37@gmail.com>
2026-07-04 12:30:15 +0000

    Added function to organize errors in utils

---
+++ src/functions/utils.js
@@ -0,0 +1,14 @@
+export const organizeErrors = errorsArray => {
+    const errorsData = {};
+
+    errorsArray.forEach(error => {
+        if (Object.keys(errorsData).includes(error['path'])) {
+            // errorsData[error['path']].push(error['msg']);
+            errorsData[error['path']] = [...errorsData[error['path']], error['msg']];
+        } else {
+            errorsData[error['path']] = [error['msg']];
+        }
+    })
+
+    return errorsData;
+}
