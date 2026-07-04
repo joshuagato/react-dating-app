946e31b3e96bc2c16b243f8934545851557c7605
Joshua Gato <joshuagato37@gmail.com>
2026-07-04 12:31:00 +0000

    Added cookie and credentials to axios

--- src/axios.js
+++ src/axios.js
@@ -1,19 +1,26 @@
+import Cookies from 'js-cookie';
 import axios from "axios";
 
 const environment = import.meta.env.VITE_REACT_APP_ENVIRONMENT;
 const baseURL = import.meta.env.VITE_REACT_APP_API_BASE_URL;
 
+axios.defaults.withCredentials = true;
+
 const unProtectedApi = axios.create({
   baseURL,
+//   withCredentials: true
 });
 
 const protectedApi = axios.create({
   baseURL,
+//   withCredentials: true
 });
 
 protectedApi.interceptors.request.use((config) => {
-    const user = JSON.parse(sessionStorage.getItem('user'));
-    const { token } = user;
+    const token = Cookies.get('token');
+    
+    // const user = JSON.parse(sessionStorage.getItem('user'));
+    // const { token } = user;
     
     if (token) {
         const modifiedConfig = { ...config };
