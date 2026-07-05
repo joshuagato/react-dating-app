66b38333cf96131ec34f863e58310b702415be18
Joshua Gato <joshuagato37@gmail.com>
2026-07-05 03:32:57 +0000

    Initialized react toast

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
 
 import './index.css';
 import App from './App.jsx';
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
                 <App />
+                <ToastContainer />
             </BrowserRouter>
         </QueryClientProvider>
     </StrictMode>,
