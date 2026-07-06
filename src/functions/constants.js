bd29aa552e3ab01e9f29ae5ef0941a45a6c3d9ab
Joshua Gato <joshuagato37@gmail.com>
2026-07-06 23:54:40 +0000

    Added constants

---
+++ src/functions/constants.js
@@ -0,0 +1,7 @@
+export const APP_NAME = 'StreamMatch';
+
+export const LOGIN_TEXT = 'Sign in to Your Account';
+export const SIGNUP_TEXT = 'Create Your Account';
+
+export const SWITCH_TO_SIGNUP_TEXT = "Don't have an account? Sign up";
+export const SWITCH_TO_LOGIN_TEXT = "Already have an account? Sign in";
