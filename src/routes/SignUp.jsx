84b388d2d710068798f3b75be1a52cf5ffa7923a
Joshua Gato <joshuagato37@gmail.com>
2026-07-06 23:55:21 +0000

    Added signup route

---
+++ src/routes/SignUp.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { CircleCheck, CircleX } from "lucide-react";
+import { toast } from 'react-toastify';
+
+import Layout from "../components/Layout";
+import Email from "../components/Email";
+import Password from "../components/Password";
+import ConfirmPassword from "../components/ConfirmPassword";
+import SubmitButton from "../components/SubmitButton";
+import SwitchContextButton from "../components/SwitchContextButton";
+
+import { signUpHandler, getProfileHandler } from '../tanstack';
+import { unsetErrorSetMessage, unsetMessageSetError, unsetEmailPasswordFields, 
+    unsetAllErrors } from "../functions/utils";
+import { SIGNUP_TEXT, SWITCH_TO_LOGIN_TEXT } from "../functions/constants";
+
+
+const SignUp = () => {
+    // const [isSignUp, setIsSignUp] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [isShowPassword, setIsShowPassword] = useState(false);
+
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+    const [errors, setErrors] = useState({});
+    
+    const { user, loading: authLoading } = { user: {}, loading: true };
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user && !authLoading) {
+            navigate.push("/");
+        }
+    }, [user, authLoading, navigate]);
+
+    async function handleAuth(event) {
+        event.preventDefault();
+
+        setLoading(true);
+        unsetAllErrors(setError, setErrors);
+
+        try {
+            const response = await signUpHandler({ email, password, passwordConfirmation });                
+
+            if (response.errors) setErrors(response.errors);
+
+            if (response.success && !response.session) {
+                unsetErrorSetMessage(setError, setMessage, response.message);
+                unsetEmailPasswordFields(setEmail, setPassword, setPasswordConfirmation);
+                toast.success(response.message, { autoClose: 5000 });
+                navigate('/verify-email');
+            } else {
+                unsetMessageSetError(setMessage, setError, response.message);
+                toast.error(response.message, { autoClose: 5000 });
+            }
+        } catch (error) {
+            console.log(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+  return (
+    <Layout heading={SIGNUP_TEXT}>
+
+        <form className="w-full flex flex-col items-center space-y-6" onSubmit={handleAuth}>
+            <Email email={email} setEmail={setEmail} errors={errors} />
+
+            <Password password={password} setPassword={setPassword} isShowPassword={isShowPassword} 
+                setIsShowPassword={setIsShowPassword} errors={errors}>
+                Password
+            </Password>
+
+            <ConfirmPassword passwordConfirmation={passwordConfirmation} setPasswordConfirmation={setPasswordConfirmation} 
+                isShowPassword={isShowPassword} errors={errors}>
+                Confirm Password
+            </ConfirmPassword>
+
+            {error && (
+                <div role="alert" className="alert alert-error fade-in">
+                    <CircleX />
+                    <span>{error}</span>
+                </div>
+            )}
+            {message && (
+                <div role="alert" className="alert alert-success fade-in">
+                    <CircleCheck />
+                    <span>{message}</span>
+                </div>
+            )}
+
+            <SubmitButton loading={loading}>
+                Sign Up
+            </SubmitButton>
+        </form>
+
+        <SwitchContextButton route={'/login'}>
+            {SWITCH_TO_LOGIN_TEXT}
+        </SwitchContextButton>
+        
+    </Layout>
+  )
+}
+
+export default SignUp;
