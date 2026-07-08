3327475a5de164d641b9133219ddca22a847490b
Joshua Gato <joshuagato37@gmail.com>
2026-07-08 15:50:12 +0000

    Added forgotten password and basic profile constants

--- src/functions/constants.js
+++ src/functions/constants.js
@@ -5,8 +5,10 @@ export const SIGNUP_TEXT = 'Create Your Account';
 
 export const SWITCH_TO_SIGNUP_TEXT = "Don't have an account? Sign up";
 export const SWITCH_TO_LOGIN_TEXT = "Already have an account? Sign in";
+export const SWITCH_TO_PASSWORD_RESET_TEXT = "Forgotten Your Password? Reset it Here!";
 
 export const VERIFY_EMAIL_TEXT = 'Enter Your Verification Code';
+export const SET_UP_BASIC_DETAILS = 'Set Up Basic Profile Details'
 
 const SIGNUP = 'signup';
 const LOGIN = 'login';
