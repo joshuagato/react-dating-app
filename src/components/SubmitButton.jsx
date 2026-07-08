5cab5de5e93f01f7f828ff20daf3d08104405012
Joshua Gato <joshuagato37@gmail.com>
2026-07-08 04:00:36 +0000

    Added new prop

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,8 @@
-const SubmitButton = ({ loading, children }) => {
+const SubmitButton = ({ loading, counting = false, children }) => {
   return (
     <button
         type="submit"
-        disabled={loading}
+        disabled={loading || counting}
         className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm 
             font-medium text-white bg-gradient-to-r from-pink-500 to-red-500 hover:from-pink-600 
             hover:to-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 
