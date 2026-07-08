5e75afe60f6a663c8f0b121f6dde0bbb2033a66e
Joshua Gato <joshuagato37@gmail.com>
2026-07-08 04:02:34 +0000

    Added verify email component

---
+++ src/routes/VerifyEmail.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react';
+import { CircleCheck, CircleX } from "lucide-react";
+import { useLocation } from "react-router";
+import { toast } from 'react-toastify';
+
+import { verifyEmailHandler } from '../tanstack/auth';
+import { unsetMessageSetError, unsetErrorSetMessage } from '../functions/utils';
+import CountdownTimer from '../components/CountdownTimer';
+import SubmitButton from '../components/SubmitButton';
+
+const Verify = () => {
+    const [loading, setLoading] = useState(false);
+    const [verification_code, setCode] = useState('');
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
+    const [counting, setCounting] = useState(false);
+
+    const location = useLocation();
+    
+    const verification_channel = location.state?.verification_channel;
+
+    async function handleVerification(event) {
+        event.preventDefault();
+
+        setLoading(true);
+        // unsetAllErrors(setError, setErrors);
+
+        try {
+            const response = await verifyEmailHandler({ verification_code, verification_channel });
+            const { success, message, exceededLimit } = response;
+
+            if (!success) {
+                
+                toast.error(message, { autoClose: 5000, theme: 'colored' });
+                unsetMessageSetError(setMessage, setError, message);
+
+                if (!exceededLimit) return setCode('');
+
+                return setCounting(true);
+            }
+
+            toast.success(message, { autoClose: 5000, theme: 'colored' });
+            unsetErrorSetMessage(setError, setMessage, message);
+            
+        } catch (error) {
+            console.log(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+    
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-emerald-200 to-cyan-800 px-2.5">
+            <div className="md:max-w-md w-full space-y-12 p-2 sm:p-8 bg-[#f8fafc] border border-[#e2e8f0] rounded-sm py-20">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold bg-gradient-to-r from-red-600 to-pink-600 bg-clip-text text-transparent mb-2">
+                        StreamMatch
+                    </h1>
+                    <p className="text-teal-400">
+                        Verify Your Email
+                    </p>
+                </div>
+
+                <form className="w-full flex flex-col items-center space-y-12" onSubmit={handleVerification}>
+                    <label className="otp otp-xl otp-success">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+
+                        <input type="text" autoComplete="one-time-code" inputMode="numeric" maxLength="4" pattern="[0-9]{4}" 
+                            value={verification_code} onChange={e => setCode(e.target.value)} required />
+                    </label>
+
+                    {error && (
+                        <div role="alert" className="alert alert-error fade-in">
+                            <CircleX />
+                            <span>{error}</span>
+                        </div>
+                    )}
+
+                    {message && (
+                        <div role="alert" className="alert alert-success fade-in">
+                            <CircleCheck />
+                            <span>{message}</span>
+                        </div>
+                    )}
+
+                    {counting && <CountdownTimer counting={counting} setCounting={setCounting} />}
+
+                    <SubmitButton loading={loading} counting={counting}>
+                        Submit Code
+                    </SubmitButton>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Verify;
