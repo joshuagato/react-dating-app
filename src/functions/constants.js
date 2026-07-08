d4cf5f09d9ae4bebca6bdfa429a0c0638e1b4703
Joshua Gato <joshuagato37@gmail.com>
2026-07-08 05:45:13 +0000

    Added constant

--- src/functions/constants.js
+++ src/functions/constants.js
@@ -6,6 +6,8 @@ export const SIGNUP_TEXT = 'Create Your Account';
 export const SWITCH_TO_SIGNUP_TEXT = "Don't have an account? Sign up";
 export const SWITCH_TO_LOGIN_TEXT = "Already have an account? Sign in";
 
+export const VERIFY_EMAIL_TEXT = 'Enter Your Verification Code';
+
 const SIGNUP = 'signup';
 const LOGIN = 'login';
 
