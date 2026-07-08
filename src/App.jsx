f45cb3311b66085f8839434cf902853191031f12
Joshua Gato <joshuagato37@gmail.com>
2026-07-08 04:00:11 +0000

    Added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from "react-router";
 import Home from './routes/Home';
 import Auth from './routes/Auth';
+import Login from './routes/Login';
+import SignUp from './routes/SignUp';
+import Verify from './routes/VerifyEmail';
+import ResetPassword from './routes/ResetPassword';
 import Others from "./routes/Others";
 
 import './App.css';
@@ -12,6 +16,10 @@ function App() {
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="auth" element={<Auth />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="verify-email" element={<Verify />} />
+        <Route path="reset-password" element={<ResetPassword />} />
         <Route path="others" element={<Others />} />
         
         {/* <Route element={<AuthLayout />}>
