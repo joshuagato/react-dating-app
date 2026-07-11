0a10f931a7551b220d5dd8fc4200a9b97caba797
Joshua Gato <joshuagato37@gmail.com>
2026-07-11 02:33:50 +0000

    Added password reset handler functions to tanstack

--- src/tanstack/auth.js
+++ src/tanstack/auth.js
@@ -19,7 +19,22 @@ const profile = async () => {
 }
 
 const verifyEmail = async verificationData => {
-    const response = await protectedApi.post('/auth/verify-email', verificationData);
+    const response = await protectedApi.patch('/auth/verify-email', verificationData);
+    return response.data;
+}
+
+const requestPasswordReset = async requestData => {
+    const response = await protectedApi.patch('/auth/request-password-reset', requestData);
+    return response.data;
+}
+
+const confirmPasswordReset = async requestData => {
+    const response = await protectedApi.post('/auth/confirm-password-reset', requestData);
+    return response.data;
+}
+
+const resetPassword = async requestData => {
+    const response = await protectedApi.post('/auth/reset-password', requestData);
     return response.data;
 }
 
@@ -27,11 +42,14 @@ const verifyEmail = async verificationData => {
 
 export const loginHandler = async data => await authentication('login', data);
 
-
 export const signUpHandler = async data => await authentication('signup', data);
-
 
 export const getProfileHandler = async () => await profile();
 
-
 export const verifyEmailHandler = async data => await verifyEmail(data);
+
+export const requestPasswordResetHandler = async data => await requestPasswordReset(data);
+
+export const confirmPasswordResetHandler = async data => await confirmPasswordReset(data);
+
+export const resetPasswordHandler = async data => await resetPassword(data);
