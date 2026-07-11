5c71ab4fce446f3f9855c34cec84bcacde2c130e
Joshua Gato <joshuagato37@gmail.com>
2026-07-11 02:30:44 +0000

    Added new constants

--- src/functions/constants.js
+++ src/functions/constants.js
@@ -6,9 +6,14 @@ export const SIGNUP_TEXT = 'Create Your Account';
 export const SWITCH_TO_SIGNUP_TEXT = "Don't have an account? Sign up";
 export const SWITCH_TO_LOGIN_TEXT = "Already have an account? Sign in";
 export const SWITCH_TO_PASSWORD_RESET_TEXT = "Forgotten Your Password? Reset it Here!";
+export const SWITCH_BACK_TO_LOGIN_TEXT = "Switch Back to Login";
 
 export const VERIFY_EMAIL_TEXT = 'Enter Your Verification Code';
 export const SET_UP_BASIC_DETAILS = 'Set Up Basic Profile Details'
+export const RESET_PASSWORD_TEXT = 'Enter Email to Receive a Password Reset Code';
+export const RESET_PASSWORD_CONFIRMATION_TEXT = 'Enter Your Request Confirmation Code';
+
+export const SET_NEW_PASSWORD_TEXT = 'Enter New Password and Confirm';
 
 const SIGNUP = 'signup';
 const LOGIN = 'login';
