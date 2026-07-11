36174f3f36074f899aa769035a1a381a8eb21251
Joshua Gato <joshuagato37@gmail.com>
2026-07-11 02:27:03 +0000

    Added helmet header component

---
+++ src/components/HelmetHeader.jsx
@@ -0,0 +1,16 @@
+import { APP_NAME } from '../functions/constants'
+import { Helmet } from 'react-helmet-async'
+
+const HelmetHeader = ({ pageTitle, seoStuff }) => {
+  return (
+    <Helmet>
+        <title>{`${pageTitle} | ${APP_NAME}`}</title>
+        {seoStuff && <link rel="notImportant" href="https://www.chipotle.com" />}
+        {seoStuff && <meta name="whatever" value="notImportant" />}
+        {seoStuff && <link rel="canonical" href="https://www.tacobell.com" />}
+        {seoStuff && <meta property="og:title" content="A very important title"/>}
+    </Helmet>
+  )
+}
+
+export default HelmetHeader
