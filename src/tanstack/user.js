64c877d4783d5617e000c581458f00fc78082eff
Joshua Gato <joshuagato37@gmail.com>
2026-07-13 16:47:57 +0000

    Added setup profile handler

--- src/tanstack/user.js
+++ src/tanstack/user.js
@@ -5,5 +5,12 @@ const getProfile = async () => {
     return response.data;
 }
 
+const setupProfile = async data => {
+    const response = await protectedApi.put('/user/profile', data);
+    return response.data;
+}
+
 
 export const getProfileHandler = async () => await getProfile();
+
+export const setupProfileHandler = async data => await setupProfile(data);
