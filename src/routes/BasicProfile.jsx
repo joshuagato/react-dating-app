8edfb1beee94bf0741085fe136856e60f2ac7a7e
Joshua Gato <joshuagato37@gmail.com>
2026-08-07 04:44:24 +0000

    Added basic profile

--- src/routes/BasicProfile.jsx
+++ src/routes/BasicProfile.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
-import { User, User2, UserCircle, CircleX, CircleCheck } from 'lucide-react'; 
+import { User, User2, UserCircle, CircleX, CircleCheck } from 'lucide-react';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router';
 
-import { SET_UP_BASIC_DETAILS } from '../functions/constants';
-import { unsetAllErrors } from '../functions/utils';
+import { SET_UP_BASIC_DETAILS, advancedProfilePath } from '../functions/constants';
+import { unsetAllErrors, unsetErrorSetMessage, unsetMessageSetError } from '../functions/utils';
 import { setupBasicProfileHandler } from '../tanstack/user';
 
-import Layout from '../components/Layout';
+import Layout from '../components/Layouts/SetupLayout';
 import Text from '../components/Text';
 import HelmetHeader from '../components/HelmetHeader';
 import Date from '../components/Date';
@@ -46,7 +46,7 @@ const BasicProfile = () => {
         unsetAllErrors(setError, setErrors);
 
         try {
-            const data = { 
+            const data = {
                 first_name, last_name, other_names, date_of_birth, gender, interested_in,
                 first_name_on: firstNameCheckBoxChecked, last_name_on: lastNameCheckBoxChecked,
                 other_names_on: otherNamesCheckBoxChecked, gender_on: genderCheckBoxChecked
@@ -56,66 +56,68 @@ const BasicProfile = () => {
             const { success, message } = response;
 
             if (success) {
+                unsetErrorSetMessage(setError, setMessage, message);
                 toast.success(message, { autoClose: 5000, theme: 'colored' });
-                navigate('/advanced-profile');
+                navigate(advancedProfilePath, { replace: true });
             } else {
+                unsetMessageSetError(setMessage, setError, message);
                 toast.error(message, { autoClose: 5000, theme: 'colored' });
             }
-            
+
         } catch (error) {
             toast.error(error.message, { autoClose: 5000, theme: 'colored' });
         } finally {
             setLoading(false);
         }
     }
-  return (
-    <Layout heading={SET_UP_BASIC_DETAILS}>
-        <HelmetHeader pageTitle={'Basic Profile'} />
-        
-        <form className="w-full flex flex-col items-center space-y-3" onSubmit={handleProfileSave}>
+    return (
+        <Layout heading={SET_UP_BASIC_DETAILS}>
+            <HelmetHeader pageTitle={'Basic Profile'} />
 
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                <legend className="fieldset-legend">Basic Details as they appear on your ID</legend>
+            <form className="w-full flex flex-col items-center space-y-3" onSubmit={handleProfileSave}>
 
-                <Text Icon={UserCircle} label={'First Name'} type={'text'} placeholder={'e.g Kenneth'} 
-                    text={first_name} setText={setFirstName} errors={errors} checkBoxDisabled={true}
-                    setCheckBoxChecked={setFirstNameCheckBoxChecked} checkBoxChecked={firstNameCheckBoxChecked} />
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">Basic Details as they appear on your ID</legend>
 
-                <Text Icon={User} label={'Last Name'} type={'text'} placeholder={'e.g Hagin'} 
-                    text={last_name} setText={setLastName} errors={errors} 
-                    setCheckBoxChecked={setLastNameCheckBoxChecked} checkBoxChecked={lastNameCheckBoxChecked} />
-                
-                <Text Icon={User2} label={'Other Names'} type={'text'} placeholder={'e.g Erwin'} required={false}
-                    text={other_names} setText={setOtherNames} errors={errors} checkBoxDisabled={!other_names}
-                    setCheckBoxChecked={setOtherNamesCheckBoxChecked} checkBoxChecked={otherNamesCheckBoxChecked} />
+                    <Text Icon={UserCircle} label={'First Name'} type={'text'} placeholder={'e.g Kenneth'}
+                        text={first_name} setText={setFirstName} errors={errors} checkBoxDisabled={true}
+                        setCheckBoxChecked={setFirstNameCheckBoxChecked} checkBoxChecked={firstNameCheckBoxChecked} />
 
-                <Date label={'Date of Birth'} date={date_of_birth} setDate={setDateOfBirth} errors={errors} />
+                    <Text Icon={User} label={'Last Name'} type={'text'} placeholder={'e.g Hagin'}
+                        text={last_name} setText={setLastName} errors={errors}
+                        setCheckBoxChecked={setLastNameCheckBoxChecked} checkBoxChecked={lastNameCheckBoxChecked} />
 
-                <Gender setCheckBoxChecked={setGenderCheckBoxChecked} checkBoxChecked={genderCheckBoxChecked}
-                    label={'Gender'} setGender={setGender}  />
+                    <Text Icon={User2} label={'Other Names'} type={'text'} placeholder={'e.g Erwin'} required={false}
+                        text={other_names} setText={setOtherNames} errors={errors} checkBoxDisabled={!other_names}
+                        setCheckBoxChecked={setOtherNamesCheckBoxChecked} checkBoxChecked={otherNamesCheckBoxChecked} />
 
-                <InterestedIn label={'Interested In'} setInterestedIn={setInterestedIn} />
+                    <Date label={'Date of Birth'} date={date_of_birth} setDate={setDateOfBirth} errors={errors} />
 
-                {error && (
-                    <div role="alert" className="alert alert-error fade-in">
-                        <CircleX />
-                        <span>{error}</span>
-                    </div>
-                )}
-                {message && (
-                    <div role="alert" className="alert alert-success fade-in">
-                        <CircleCheck />
-                        <span>{message}</span>
-                    </div>
-                )}
+                    <Gender setCheckBoxChecked={setGenderCheckBoxChecked} checkBoxChecked={genderCheckBoxChecked}
+                        label={'Gender'} setGender={setGender} />
 
-                <SubmitButton loading={loading}>
-                    Save
-                </SubmitButton>
-            </fieldset>
-        </form>
-    </Layout>
-  )
+                    <InterestedIn label={'Interested In'} setInterestedIn={setInterestedIn} />
+
+                    {error && (
+                        <div role="alert" className="alert alert-error fade-in">
+                            <CircleX />
+                            <span>{error}</span>
+                        </div>
+                    )}
+                    {message && (
+                        <div role="alert" className="alert alert-success fade-in">
+                            <CircleCheck />
+                            <span>{message}</span>
+                        </div>
+                    )}
+
+                    <SubmitButton loading={loading}>
+                        Save
+                    </SubmitButton>
+                </fieldset>
+            </form>
+        </Layout>
+    )
 }
 
 export default BasicProfile;
