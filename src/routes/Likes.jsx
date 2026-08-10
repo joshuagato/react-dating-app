2eb612e9f0befbee910bea2356ee61d910c118a3
Joshua Gato <joshuagato37@gmail.com>
2026-08-10 07:10:12 +0000

    Added likes

---
+++ src/routes/Likes.jsx
@@ -0,0 +1,57 @@
+import { useState, useRef, useCallback, useEffect } from 'react';
+
+import { LIKES_TITLE, LIKES_TEXT } from '../functions/constants';
+import { getPotentialMatchProfilesHandler } from '../tanstack/user';
+
+import MainLayout from '../components/Layouts/MainLayout';
+import HelmetHeader from '../components/HelmetHeader';
+
+export default function Likes() {
+    const [profiles, setProfiles] = useState([]);
+
+    // Fetch potential matches on mount
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await getPotentialMatchProfilesHandler();
+                const fetchedProfiles = response?.userProfiles || [];
+
+                if (fetchedProfiles.length > 0) {
+                    setProfiles(fetchedProfiles);
+                }
+            } catch (error) {
+                console.error("Failed to fetch match profiles:", error);
+            }
+        })();
+    }, []);
+
+    useEffect(() => {
+        // console.log({ profiles });
+    }, [profiles]);
+
+    return (
+        <MainLayout pageTitle={LIKES_TITLE} pageDetails={LIKES_TEXT}>
+            <HelmetHeader pageTitle={LIKES_TITLE} />
+
+            <div className="relative w-full h-full flex flex-col overflow-hidden select-none fade-in overflow-y-scroll">
+                <div className='w-full h-full grid max-sm:grid-cols-2  max-lg:grid-cols-3 gap-3 lg:grid-cols-2'>
+                    {profiles.length > 0 && profiles.map((profile, index) => {
+                        return (
+                            <article key={index} className='w-full h-[calc(200px + 50px)] sm:h-[calc(280px + 70px)]'>
+                                <div className='w-full h-50 sm:h-70'>
+                                    <img className='w-full h-full object-cover' src={profile.pictures[0]} alt="User Profile Picture" />
+                                </div>
+                                <div className='w-full h-12.5 sm:h-17.5 px-2 py-1 bg-gradient-to-t from-black/90 to-black/80'>
+                                    <div className='w-full h-full flex flex-col justify-center items-center bg-clip-text text-transparent bg-gradient-to-r from-violet-600 to-pink-600'>
+                                        <h1 className='text-[14px] sm:text-[18px]'>{profile.name}, {profile.age}</h1>
+                                        <p className='text-[12px] sm:text-[14px]'>{profile.liked} ago</p>
+                                    </div>
+                                </div>
+                            </article>
+                        );
+                    })}
+                </div>
+            </div>
+        </MainLayout>
+    );
+}
