adc6207b6868a3236ba7c51000e1f9af62a02fe8
Joshua Gato <joshuagato37@gmail.com>
2026-08-15 21:56:59 +0000

    Updated imports and reformatted file

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
-import { lastArrayElement, makeLabelTextId } from '../functions/utils';
-import { enabledColor, disabledColor } from "../functions/constants";
+import { lastArrayElement, makeLabelTextId } from '../utils/functions';
+import { enabledColor, disabledColor } from "../utils/constants";
 
-const Text = ({ Icon, label, type, placeholder, text, setText, checkBoxChecked=false, 
-    setCheckBoxChecked, checkBoxDisabled=false, required=true, errors }) => {
+const Text = ({ Icon, label, type, placeholder, text, setText, checkBoxChecked = false,
+    setCheckBoxChecked, checkBoxDisabled = false, required = true, errors }) => {
 
     const [checkboxLabelColor, setCheckboxLabelColor] = useState(disabledColor);
 
@@ -23,11 +23,11 @@ const Text = ({ Icon, label, type, placeholder, text, setText, checkBoxChecked=f
                     htmlFor={fieldId}
                     className="block self-start text-sm font-medium text-purple-900"
                 >
-                    { label } {required && <span className='text-red-600'>*</span>}
+                    {label} {required && <span className='text-red-600'>*</span>}
                 </label>
                 <div>
                     <span className={`text-[10px] ${checkboxLabelColor}`}>Display on Profile</span>
-                    <input type="checkbox" checked={checkBoxChecked} onChange={e => setCheckBoxChecked(e.target.checked)} 
+                    <input type="checkbox" checked={checkBoxChecked} onChange={e => setCheckBoxChecked(e.target.checked)}
                         disabled={checkBoxDisabled} className="toggle toggle-xs" />
                 </div>
             </div>
@@ -38,10 +38,10 @@ const Text = ({ Icon, label, type, placeholder, text, setText, checkBoxChecked=f
                         value={text} onChange={(e) => setText(e.target.value)} required={required} />
                 </label>
                 {errors[fieldId] && (
-                <div className="text-red-400 text-sm text-center mt-1.5 fade-in">
-                    {lastArrayElement(errors[fieldId])}
-                </div>
-            )}
+                    <div className="text-red-400 text-sm text-center mt-1.5 fade-in">
+                        {lastArrayElement(errors[fieldId])}
+                    </div>
+                )}
             </div>
         </>
     )
