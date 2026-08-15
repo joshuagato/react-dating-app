ddffc8d893af7227f3dcc6c8fc32168ba26e0461
Joshua Gato <joshuagato37@gmail.com>
2026-08-15 22:04:42 +0000

    Updated imports and reformatted file

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,29 +1,29 @@
 import { CalendarDays } from 'lucide-react';
-import { lastArrayElement, makeLabelTextId } from '../functions/utils';
+import { lastArrayElement, makeLabelTextId } from '../utils/functions';
 
 const Date = ({ label, date, setDate, errors }) => {
-    
+
     const fieldId = makeLabelTextId(label);
-    
+
     return (
         <>
             <label
                 htmlFor={fieldId}
                 className="block self-start text-sm font-medium text-purple-900"
             >
-                { label } {<span className='text-red-600'>*</span>}
+                {label} {<span className='text-red-600'>*</span>}
             </label>
             <div className='mb-1.5'>
                 <label className="input">
                     <CalendarDays className="h-[1em] opacity-50" />
-                    <input id={fieldId} type="date" className="input w-58 sm:w-72" value={date} 
+                    <input id={fieldId} type="date" className="input w-58 sm:w-72" value={date}
                         onChange={e => setDate(e.target.value)} max="2017-12-31" required />
                 </label>
                 {errors[fieldId] && (
-                <div className="text-red-400 text-sm text-center mt-1.5 fade-in">
-                    {lastArrayElement(errors[fieldId])}
-                </div>
-            )}
+                    <div className="text-red-400 text-sm text-center mt-1.5 fade-in">
+                        {lastArrayElement(errors[fieldId])}
+                    </div>
+                )}
             </div>
         </>
     )
--- src/components/Gender.jsx
+++ src/components/Gender.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { makeLabelTextId } from "../functions/utils";
-import { enabledColor, disabledColor } from "../functions/constants";
+import { makeLabelTextId } from "../utils/functions";
+import { enabledColor, disabledColor } from "../utils/constants";
 
 const Gender = ({ label, setGender, checkBoxChecked, checkBoxDisabled, setCheckBoxChecked }) => {
 
     const [checkboxLabelColor, setCheckboxLabelColor] = useState(disabledColor);
-    
+
     const fieldId = makeLabelTextId(label);
 
     useEffect(() => {
@@ -16,30 +16,30 @@ const Gender = ({ label, setGender, checkBoxChecked, checkBoxDisabled, setCheckB
     }, [checkBoxDisabled]);
 
     return (
-    <>
-        <div className='mb-1.5'>
-            <div className='flex items-center justify-between'>
-                <label
-                    htmlFor={fieldId}
-                    className="block self-start text-sm font-medium text-purple-900"
-                >
-                    {label} {<span className='text-red-600'>*</span>}
-                </label>
-                <div>
-                    <span className={`text-[10px] ${checkboxLabelColor}`}>Display on Profile</span>
-                    <input type="checkbox" checked={checkBoxChecked} onChange={e => setCheckBoxChecked(e.target.checked)} 
-                        disabled={checkBoxDisabled} className="toggle toggle-xs" />
+        <>
+            <div className='mb-1.5'>
+                <div className='flex items-center justify-between'>
+                    <label
+                        htmlFor={fieldId}
+                        className="block self-start text-sm font-medium text-purple-900"
+                    >
+                        {label} {<span className='text-red-600'>*</span>}
+                    </label>
+                    <div>
+                        <span className={`text-[10px] ${checkboxLabelColor}`}>Display on Profile</span>
+                        <input type="checkbox" checked={checkBoxChecked} onChange={e => setCheckBoxChecked(e.target.checked)}
+                            disabled={checkBoxDisabled} className="toggle toggle-xs" />
+                    </div>
+                </div>
+                <div className="gender">
+                    <input className="gender-item btn w-25" value={'man'} type="radio" name={fieldId} aria-label="Man"
+                        onChange={e => setGender(e.target.value)} required />
+                    <input id={fieldId} className="gender-item btn w-25" value={'woman'} type="radio" name={fieldId} aria-label="Woman"
+                        onChange={e => setGender(e.target.value)} required />
                 </div>
             </div>
-            <div className="gender">
-                <input className="gender-item btn w-25" value={'man'} type="radio" name={fieldId} aria-label="Man"
-                    onChange={e => setGender(e.target.value)} required />
-                <input id={fieldId} className="gender-item btn w-25" value={'woman'} type="radio" name={fieldId} aria-label="Woman"
-                    onChange={e => setGender(e.target.value)} required />
-            </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Gender;
