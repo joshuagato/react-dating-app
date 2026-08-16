8e53f6c4ef850b20f92bac91c1f7292a283eae00
Joshua Gato <joshuagato37@gmail.com>
2026-08-16 19:26:20 +0000

    Added chat bubbles

--- src/routes/Others.jsx
+++ src/routes/Others.jsx
@@ -191,10 +191,41 @@ function Others() {
             <p>.</p><p>.</p><p>.</p>
 
 
+            <div className="chat chat-start">
+                <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+            </div>
+            <div className="chat chat-start">
+                <div className="chat-bubble chat-bubble-secondary">
+                    Put me on the Council and not make me a Master!??
+                </div>
+            </div>
+            <div className="chat chat-start">
+                <div className="chat-bubble chat-bubble-accent">
+                    That's never been done in the history of the Jedi.
+                </div>
+            </div>
+            <div className="chat chat-start">
+                <div className="chat-bubble chat-bubble-neutral">It's insulting!</div>
+            </div>
+            <div className="chat chat-end">
+                <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+            </div>
+            <div className="chat chat-end">
+                <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
+            </div>
+            <div className="chat chat-end">
+                <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
+            </div>
+            <div className="chat chat-end">
+                <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
+            </div>
+
 
             <p>.</p><p>.</p><p>.</p>
 
-
+            <div className="tooltip" data-tip="hello">
+                <button className="btn">Hover me</button>
+            </div>
 
             <p>.</p><p>.</p><p>.</p>
 
