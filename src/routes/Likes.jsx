c4973162893d0be6965b5282b3a1e76e110618c5
Joshua Gato <joshuagato37@gmail.com>
2026-08-18 18:57:54 +0000

    Added scroll-bar class

--- src/routes/Likes.jsx
+++ src/routes/Likes.jsx
@@ -38,7 +38,7 @@ export default function Likes() {
         <MainLayout pageTitle={LIKES_TITLE} pageDetails={LIKES_TEXT}>
             <HelmetHeader pageTitle={LIKES_TITLE} />
 
-            <div className="relative w-full h-full flex flex-col overflow-hidden select-none fade-in px-4 sm:px-8 py-2 overflow-y-scroll">
+            <div className="relative w-full h-full flex flex-col overflow-hidden select-none fade-in px-4 sm:px-8 py-2 scroll-bar">
                 <div className='w-full h-full grid max-sm:grid-cols-2  max-lg:grid-cols-3 gap-3 lg:grid-cols-2'>
                     {profiles.length > 0 && profiles.map((profile, index) => {
                         return (
