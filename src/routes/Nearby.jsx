fcd9697c3d93f1fe7876a3b689aa9b8fda16e090
Joshua Gato <joshuagato37@gmail.com>
2026-08-19 03:24:30 +0000

    Added scrollbar

--- src/routes/Nearby.jsx
+++ src/routes/Nearby.jsx
@@ -34,7 +34,7 @@ export default function Nearby() {
         <MainLayout pageTitle={NEARBY_TITLE} pageDetails={NEARBY_TEXT}>
             <HelmetHeader pageTitle={NEARBY_TITLE} />
 
-            <div className="relative w-full h-full flex flex-col overflow-hidden select-none fade-in px-4 sm:px-8 py-2 overflow-y-scroll">
+            <div className="relative w-full h-full flex flex-col overflow-hidden select-none fade-in px-4 sm:px-8 py-2 scroll-bar">
                 <div className='w-full h-full grid max-sm:grid-cols-2  max-lg:grid-cols-3 gap-3 lg:grid-cols-2'>
                     {profiles.length > 0 && profiles.map((profile, index) => {
                         return (
