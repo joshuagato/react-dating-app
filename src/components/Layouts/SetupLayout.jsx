cc24a576401c16d39f8c11b6baa3602c88db5421
Joshua Gato <joshuagato37@gmail.com>
2026-08-20 20:06:34 +0000

    Removed fade-in class

--- src/components/Layouts/SetupLayout.jsx
+++ src/components/Layouts/SetupLayout.jsx
@@ -5,7 +5,7 @@ import { APP_NAME } from '../../utils/constants';
 const SetupLayout = ({ children, heading }) => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-emerald-200 to-cyan-800 px-4 py-2">
-            <div className="md:max-w-md w-full space-y-8 px-4 sm:px-8 py-6 sm:py-20 bg-[#f8fafc] border border-[#e2e8f0] rounded-sm fade-in">
+            <div className="md:max-w-md w-full space-y-8 px-4 sm:px-8 py-6 sm:py-20 bg-[#f8fafc] border border-[#e2e8f0] rounded-sm">
                 <div className="text-center">
                     <h1 className="text-4xl font-bold bg-gradient-to-r from-red-600 to-pink-600 bg-clip-text 
                     text-transparent mb-2">
