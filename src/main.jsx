446e3d053d6dec8abc5e0f81ed8964a56c04b5a1
Joshua Gato <joshuagato37@gmail.com>
2026-09-03 03:30:22 +0000

    Added google auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,24 @@ import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify';
 import { HelmetProvider } from 'react-helmet-async';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import './index.css';
 import App from './App.jsx';
 
 const queryClient = new QueryClient()
+// const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "308055312006-tkaegbj35o95b7o24p785m25cs3rgjtd.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
-    
+
     <StrictMode>
         <HelmetProvider>
             <QueryClientProvider client={queryClient}>
                 <BrowserRouter>
-                    <App />
+                    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+                        <App />
+                    </GoogleOAuthProvider>
                     <ToastContainer />
                 </BrowserRouter>
             </QueryClientProvider>
