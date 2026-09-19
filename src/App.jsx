41b1ed6f9182ea2aa285a2592c2bd55be1d2b075
Joshua Gato <joshuagato37@gmail.com>
2026-09-19 17:34:04 +0000

    Implemented protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import BasicProfile from './routes/BasicProfile';
 import AdvancedProfile from './routes/AdvancedProfile';
 import FinalProfile from './routes/FinalProfile';
 import Encounters from './routes/Encounters';
-// import Matcher from './routes/Matcher';
 import Likes from './routes/Likes';
 import Nearby from './routes/Nearby';
 import Chats from './routes/Chats';
@@ -18,10 +17,10 @@ import Chat from './routes/Chat';
 import Profile from './routes/Profile';
 import InstallApp from './routes/InstallApp';
 import PremiumPackages from './routes/PremiumPackages';
-// import FaceDetector from './routes/FaceDetector';
 import ProfilePageSetup from './routes/ProfilePageSetup';
 import PartnerProfile from './routes/PartnerProfile';
 import Others from "./routes/Others";
+import ProtectedRoute from "./components/ProtectedRoute"; // 👈 Import Guard Component
 import { PWAProvider } from './components/PWAContext';
 import { baseURL, userId } from './utils/constants';
 import { connectSocket } from './utils/functions';
@@ -34,43 +33,35 @@ function App() {
     return (
         <PWAProvider>
             <Routes>
+                {/* Unprotected / Public Routes (First 6) */}
                 <Route path="/" element={<Home />} />
                 <Route path="login" element={<Login />} />
                 <Route path="signup" element={<SignUp />} />
                 <Route path="verify-email" element={<Verify />} />
                 <Route path="reset-password" element={<ResetPassword />} />
                 <Route path="confirm-reset-password" element={<ConfirmPasswordReset />} />
-                <Route path="set-new-password" element={<SetNewPassword />} />
-                <Route path="basic-profile" element={<BasicProfile />} />
-                <Route path="advanced-profile" element={<AdvancedProfile />} />
-                <Route path="final-profile" element={<FinalProfile />} />
-                <Route path="profile-page" element={<ProfilePageSetup />} />
-                <Route path="partner-profile" element={<PartnerProfile />} />
-                <Route path="encounters" element={<Encounters />} />
-                <Route path="likes" element={<Likes />} />
-                <Route path="nearby" element={<Nearby />} />
-                <Route path="chats" element={<Chats />} />
-                <Route path="chat" element={<Chat />} />
-                <Route path="profile" element={<Profile />} />
-                {/* <Route path="matcher" element={<Matcher />} /> */}
-                <Route path="install" element={<InstallApp />} />
-                <Route path="premium" element={<PremiumPackages />} />
-                {/* <Route path="face" element={<FaceDetector />} /> */}
-                <Route path="others" element={<Others />} />
 
-                {/* <Route element={<AuthLayout />}>
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
+                {/* Protected Routes (Require Authentication) */}
+                <Route element={<ProtectedRoute />}>
+                    <Route path="set-new-password" element={<SetNewPassword />} />
+                    <Route path="basic-profile" element={<BasicProfile />} />
+                    <Route path="advanced-profile" element={<AdvancedProfile />} />
+                    <Route path="final-profile" element={<FinalProfile />} />
+                    <Route path="profile-page" element={<ProfilePageSetup />} />
+                    <Route path="partner-profile" element={<PartnerProfile />} />
+                    <Route path="encounters" element={<Encounters />} />
+                    <Route path="likes" element={<Likes />} />
+                    <Route path="nearby" element={<Nearby />} />
+                    <Route path="chats" element={<Chats />} />
+                    <Route path="chat" element={<Chat />} />
+                    <Route path="profile" element={<Profile />} />
+                    <Route path="install" element={<InstallApp />} />
+                    <Route path="premium" element={<PremiumPackages />} />
+                    <Route path="others" element={<Others />} />
                 </Route>
-
-                <Route path="concerts">
-                    <Route index element={<ConcertsHome />} />
-                    <Route path=":city" element={<City />} />
-                    <Route path="trending" element={<Trending />} />
-                </Route> */}
             </Routes>
         </PWAProvider>
-    )
+    );
 }
 
-export default App
+export default App;
