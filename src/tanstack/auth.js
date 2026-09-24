ae6e63ba2b2f871a698a29b9e63adc813435cb7c
Joshua Gato <joshuagato37@gmail.com>
2026-09-24 15:26:10 +0000

    Added get setup status handler function

--- src/tanstack/auth.js
+++ src/tanstack/auth.js
@@ -1,6 +1,6 @@
 import { unProtectedApi, protectedApi } from "../axios";
 
-const googleAuth = async data => {
+export const googleAuthHandler = async data => {
     const response = await unProtectedApi.post('/auth/google-auth', data);
     return response.data;
 }
@@ -18,38 +18,32 @@ const authentication = async (operation, authData) => {
     return response.data;
 }
 
-const verifyEmail = async verificationData => {
+export const verifyEmailHandler = async verificationData => {
     const response = await protectedApi.patch('/auth/verify-email', verificationData);
     return response.data;
 }
 
-const requestPasswordReset = async requestData => {
+export const requestPasswordResetHandler = async requestData => {
     const response = await unProtectedApi.patch('/auth/request-password-reset', requestData);
     return response.data;
 }
 
-const confirmPasswordReset = async requestData => {
+export const confirmPasswordResetHandler = async requestData => {
     const response = await protectedApi.post('/auth/confirm-password-reset', requestData);
     return response.data;
 }
 
-const resetPassword = async requestData => {
+export const resetPasswordHandler = async requestData => {
     const response = await protectedApi.post('/auth/reset-password', requestData);
     return response.data;
 }
 
+export const getSetupStatusHandler = async () => {
+    const response = await protectedApi.get('/auth/setup-status');
+    return response.data;
+};
 
-
-export const googleAuthHandler = async data => await googleAuth(data);
 
 export const loginHandler = async data => await authentication('login', data);
 
 export const signUpHandler = async data => await authentication('signup', data);
-
-export const verifyEmailHandler = async data => await verifyEmail(data);
-
-export const requestPasswordResetHandler = async data => await requestPasswordReset(data);
-
-export const confirmPasswordResetHandler = async data => await confirmPasswordReset(data);
-
-export const resetPasswordHandler = async data => await resetPassword(data);
